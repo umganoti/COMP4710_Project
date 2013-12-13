d00b1d952d56e60242c3d1ac582b98816a46e649
Ian Armit <umarmiti@cc.umanitoba.ca>
2013-12-13 15:50:05 -0600

    Added candidate.cpp and made sure I knew hot to use makefiles and classes with test.cpp

---
+++ Candidate.cpp
@@ -0,0 +1,8 @@
+#include "Candidate.h"
+
+Candidate::Candidate(int lmv, int tmv_size)
+{
+    m_lmv = lmv;
+    m_tmv = new int[tmv_size];
+}
+
---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>  //stream IO
+#include <fstream>   //file IO
+#include <string>    //strings
+#include <sstream> //stringstreams for converting data.
+#include <stdlib.h>
+#include <vector>
+#include "Candidate.h"
+using namespace std;
+
+//Main here opens a file and reads individual characters
+int main(void) {
+
+    cout << "Hello\n";
+    Candidate test (12, 12);
+    cout << test.get_lmv() << "\n";
+    return (1);
+}
